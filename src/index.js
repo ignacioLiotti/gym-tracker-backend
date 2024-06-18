eb85b32f3e751c7bf01b5cddef3d7ec61b518de1
ignacioLiotti <ignacioliotti@zequenze.com>
2024-06-18 19:19:14 -0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const exerciseRoutes = require("./routes/exerciseRoutes");
 const routineRoutes = require("./routes/routineRoutes");
 const seriesRoutes = require("./routes/seriesRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.use(cors()); // Configurar CORS
-app.use(bodyParser.json());
+// Configure CORS
+app.use(
+	cors({
+		origin: "https://gym-tracker-frontend.vercel.app", // Replace with your frontend URL
+		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+		credentials: true,
+	})
+);
+
+app.use(express.json());
 
 app.use("/api/exercises", exerciseRoutes);
 app.use("/api/routines", routineRoutes);
 app.use("/api/series", seriesRoutes);
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
